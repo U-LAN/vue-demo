b7aec3fe2039ab260869749170095f05767b511b
lidongsheng <lidongsheng@dataexa.com>
2019-11-19 16:29:55 +0800

    尾递归

--- src/views/es6/components/tail-recursion.vue
+++ src/views/es6/components/tail-recursion.vue
@@ -22,6 +22,7 @@
       }
 
       factorial(5) // 120
+
       ---------------------------------------------------------
 
       上面代码是一个阶乘函数，计算n的阶乘，最多需要保存n个调用记录，复杂度 O(n) 。
@@ -36,29 +37,129 @@
       }
 
       factorial(5, 1) // 120
+    </pre>
+    <el-divider></el-divider>
+    <h4>蹦床函数：</h4>
+    <pre>
+      （一种实现：）
 
-      ---------------------------------------------------------
-
-      还有一个比较著名的例子，就是计算 Fibonacci 数列，也能充分说明尾递归优化的重要性。
-
-      非尾递归的 Fibonacci 数列实现如下。
-
-      ---------------------------------------------------------
-
-      function Fibonacci (n) {
-        if ( n <= 1 ) {return 1};
-
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
+      function trampoline(f) {
+        while (f && f instanceof Function) {
+          f = f();
+        }
+        return f;
       }
 
-      Fibonacci(10) // 89
-      Fibonacci(100) // 超时
-      Fibonacci(500) // 超时
+      trampoline(func)   //  func就是递归函数
+
+      ---------------------------------------------------------
+
+      现在，使用蹦床函数执行sum，就不会发生调用栈溢出。
+      蹦床函数并不是真正的尾递归优化，下面的实现才是。
+    </pre>
+    <el-divider></el-divider>
+    <h4>尾递归优化：</h4>
+    <pre>
+      function tco(f) {
+        var value;
+        var active = false;
+        var accumulated = [];
+
+        return function accumulator() {
+          accumulated.push(arguments);
+          if (!active) {
+            active = true;
+            while (accumulated.length) {
+              value = f.apply(this, accumulated.shift());
+            }
+            active = false;
+            return value;
+          }
+        };
+      }
+
+      var sum = tco(function(x, y) {
+        if (y > 0) {
+          return sum(x + 1, y - 1)
+        }
+        else {
+          return x
+        }
+      });
+
+      sum(1, 100000)
+
+      ---------------------------------------------------------
+
+      上面代码中，tco函数是尾递归优化的实现，它的奥妙就在于状态变量active。
+      默认情况下，这个变量是不激活的。一旦进入尾递归优化的过程，这个变量就激活了。
+      然后，每一轮递归sum返回的都是undefined，所以就避免了递归执行；
+      而accumulated数组存放每一轮sum执行的参数，总是有值的，
+      这就保证了accumulator函数内部的while循环总是会执行。
+      这样就很巧妙地将“递归”改成了“循环”，而后一轮的参数会取代前一轮的参数，保证了调用栈只有一层。
     </pre>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  mounted() {
+    this.aaa();
+  },
+  methods: {
+    aaa() {
+      function test() {
+        function trampoline(f) {
+          while (f && f instanceof Function) {
+            f = f();
+          }
+          return f;
+        }
+
+        function sum(x, y) {
+          if (y > 0) {
+            return sum.bind(null, x + 1, y - 1);
+          } else {
+            return x;
+          }
+        }
+
+        console.log(trampoline(sum(1, 100000)));
+      }
+
+      test();
+
+      //尾递归优化
+      // function tco(f) {
+      //   var value;
+      //   var active = false;
+      //   var accumulated = [];
+
+      //   return function accumulator() {
+      //     accumulated.push(arguments);
+      //     if (!active) {
+      //       active = true;
+      //       while (accumulated.length) {
+      //         value = f.apply(this, accumulated.shift());
+      //       }
+      //       active = false;
+      //       return value;
+      //     }
+      //   };
+      // }
+
+      // var sum = tco(function(x, y) {
+      //   console.log(x, y);
+      //   if (y > 0) {
+      //     return sum(x + 1, y - 1);
+      //   } else {
+      //     return x;
+      //   }
+      // });
+
+      // console.log(sum(1, 100000));
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 .title {
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,17 +1,15 @@
 <template>
   <el-container id="main">
     <el-header>
-      <h1>
-        <a href="https://cn.vuejs.org/">VUE</a> DEMO COLLECTION
-      </h1>
+      <h1><a href="https://cn.vuejs.org/">VUE</a> DEMO COLLECTION</h1>
     </el-header>
-    <el-container>
+    <el-container class="all">
       <el-aside width="200px" height="100%">
         <el-tabs tab-position="left" v-model="activeName" @tab-click="tabClick">
           <el-tab-pane
             :name="item.name"
             :label="item.cname"
-            v-for="(item,index) in navlist"
+            v-for="(item, index) in navlist"
             v-bind:key="index"
           ></el-tab-pane>
         </el-tabs>
@@ -19,18 +17,31 @@
       <el-main>
         <router-view></router-view>
       </el-main>
+      <div class="shadow">
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     </el-container>
+    <div class="ccc">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+    </div>
   </el-container>
 </template>
 
 <script>
-import {navlist} from "../../assest/dateJs/index/index"
+import { navlist } from "../../assest/dateJs/index/index";
 export default {
   name: "Index",
   data() {
     return {
       activeName: this.$route.name,
-      navlist:navlist
+      navlist: navlist
     };
   },
   methods: {
@@ -68,6 +79,78 @@ export default {
   .el-main {
     height: 870px;
     overflow: auto;
+  }
+  .all {
+    position: relative;
+    .shadow {
+      pointer-events: none;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: rgba(255, 255, 255, 0.1);
+      z-index: 99;
+      ul {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        position: relative;
+        overflow: hidden;
+        li {
+          list-style: none;
+          position: absolute;
+          left: -10px;
+          width: 200px;
+          height: 4px;
+          border-radius: 2px;
+          background-image: linear-gradient(
+            to right,
+            rgba(255, 255, 222, 0),
+            rgba(255, 255, 222, 1)
+          );
+          transform: rotate(45deg);
+          -ms-transform: rotate(45deg); /* IE 9 */
+          -webkit-transform: rotate(45deg); /* Safari and Chrome */
+          animation: movee 5s linear infinite;
+          -webkit-animation: movee 5s linear infinite;
+        }
+      }
+    }
+  }
+}
+
+@keyframes movee {
+  from {
+    left: -100px;
+    top: -100px;
+  }
+
+  to {
+    left: 100%;
+    top: 100%;
+  }
+}
+
+@-webkit-keyframes movee
+
+/* Safari and Chrome */ {
+  from {
+    left: -100px;
+    top: -100px;
+  }
+
+  to {
+    left: 100%;
+    top: 100%;
+  }
+}
+.ccc {
+  div {
+    width: 20px;
+    height: 20px;
+    border: 1px solid #000;
+    display: inline-block;
+    margin: auto;
   }
 }
 </style>
