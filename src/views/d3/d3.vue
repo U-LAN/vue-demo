cb2d9c97a4d13630b12b3a6d471b7c9bdb5c33b4
李东升 <lidongsheng@dataexa.com>
2020-03-04 17:51:53 +0800

    修改d3大小

--- src/views/d3/d3.vue
+++ src/views/d3/d3.vue
@@ -9,22 +9,22 @@
   </div>
 </template>
 <script>
-import particles from "./demos/particles";
-import spacetime from "./demos/spacetime";
+import particles from './demos/particles'
+import spacetime from './demos/spacetime'
 export default {
   components: {
     particles,
     spacetime
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 .d3 {
   width: 100%;
   height: 100%;
   .item {
-    width: 300px;
-    height: 300px;
+    width: 500px;
+    height: 500px;
     background: #fff;
     box-shadow: 0 0 5px 5px rgba(3, 8, 82, 0.1);
     margin: 10px;
--- src/views/d3/demos/particles.vue
+++ src/views/d3/demos/particles.vue
@@ -2,53 +2,53 @@
   <div class="particles"></div>
 </template>
 <script>
-import * as d3 from "d3";
+import * as d3 from 'd3'
 export default {
-  mounted() {
+  mounted () {
     // this.$nextTick(() => {
-    this.initD3();
+    this.initD3()
     // });
   },
   methods: {
-    initD3() {
-      var width = 300,
-        height = 300;
+    initD3 () {
+      var width = 500
+      var height = 500
 
-      var i = 0;
+      var i = 0
 
       var svg = d3
-        .select(".particles")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        .select('.particles')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
 
       svg
-        .append("rect")
-        .attr("width", width)
-        .attr("height", height)
-        .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+        .append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .on('ontouchstart' in document ? 'touchmove' : 'mousemove', particle)
 
-      function particle() {
-        var m = d3.mouse(this);
+      function particle () {
+        var m = d3.mouse(this)
         svg
-          .insert("circle", "rect")
-          .attr("cx", m[0])
-          .attr("cy", m[1])
-          .attr("r", 1e-6)
-          .style("stroke", d3.hsl((i = (i + 1) % 360), 1, 0.5))
-          .style("stroke-opacity", 1)
+          .insert('circle', 'rect')
+          .attr('cx', m[0])
+          .attr('cy', m[1])
+          .attr('r', 1e-6)
+          .style('stroke', d3.hsl((i = (i + 1) % 360), 1, 0.5))
+          .style('stroke-opacity', 1)
           .transition()
           .duration(2000)
           .ease(Math.sqrt)
-          .attr("r", 100)
-          .style("stroke-opacity", 1e-6)
-          .remove();
+          .attr('r', 100)
+          .style('stroke-opacity', 1e-6)
+          .remove()
 
-        d3.event.preventDefault();
+        d3.event.preventDefault()
       }
     }
   }
-};
+}
 </script>
 <style >
 .particles {
